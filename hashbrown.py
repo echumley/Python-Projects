0c547533970ebb19183e3a6753ce05fccac846c4
Ethan C <ChumleyEthan1310@gmail.com>
2024-09-09 20:35:24 -0700

    Initial commit of ongoing projects.

---
+++ hashbrown.py
@@ -0,0 +1,5 @@
+import hashlib
+
+print(hashlib.sha256(b'test').hexdigest())
+
+#print(hashlib.sha256(b'test').hexdigest())
---
+++ luhnsalgorithm.py
@@ -0,0 +1,19 @@
+# This program takes in a 16-digit number to verify if it's a valid credit card number based on Luhn's Algorithm
+numbers = 0
+
+def luhns_alg(cardNumber):
+    if len(cardNumber) == 16:
+        for num in cardNumber:
+            
+
+        
+'''
+try:
+    userIn = input('Enter a 16-digit number: ')
+
+    floatIn = float(userIn)
+    intIn = int(floatIn)
+
+except Exception as err:
+    print('Invalid input')
+'''
