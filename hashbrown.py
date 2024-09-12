2ec6a254a622aaf1cc5bc156f6a2f3876eafd2fc
Ethan C <ChumleyEthan1310@gmail.com>
2024-09-11 21:53:07 -0700

    Added support for MD5 and SHA1, plus made the code more concise.

--- hashbrown.py
+++ hashbrown.py
@@ -2,27 +2,43 @@ import os
 import hashlib
 from pathlib import Path
 
+def hashFile(target, algorithm):
+    fileContents = target.read()
+    md5obj = algorithm()
+    md5obj.update(fileContents)
+    return md5obj.hexdigest()
+
+algorithms = {
+     'MD5': hashlib.md5,
+     'SHA1': hashlib.sha1,
+     'SHA256': hashlib.sha256
+}
+
 # Requests a file from within the current directory, hashes it, and compares it to an expected hash.
 while True:
-    userInput = input('Enter a file to hash (or type "q[ENTER]" to exit): ')
-
-    if userInput == 'q':
+    algorithm = input('Pick a hashing algorithm (MD5, SHA1, or SHA256) or enter "q" to exit: ').upper()
+    if algorithm == 'Q':
         break
-    else:
+
+    userInput = input('Enter a file to hash or enter "q" to exit: ')
+    if userInput.lower() == 'q':
+        break
+
+    if algorithm in algorithms:
         try: 
             with open(userInput, 'rb') as target:
-                fileContents = target.read()
-
-                # Initializes SHA-256 hashing function
-                sha256obj = hashlib.sha256()
-                sha256obj.update(fileContents)
-                hexDigest = sha256obj.hexdigest()
-
-            expectedHash = input('Enter expected file hash: ')
-
-            if expectedHash == hexDigest:
-                print("Match! Both file hashes are the same.")
-            else:
-                print("Uh oh! These file hashes don't match. Ensure you selected the right file and inserted the correct hash.")
+                    hexDigest = hashFile(target, algorithms[algorithm])
+                    print(f'{algorithm} hash: {hexDigest}')
         except FileNotFoundError:
-            print(f'File not found: {userInput}')
+            print(f"File: {userInput} does not exist. Please try again.")
+        except Exception as err:
+            print(f'Error: {err}')
+    else:
+        print('Invalid hash algorithm.')
+        
+    expectedHash = input('Enter expected file hash: ')
+
+    if expectedHash == hexDigest:
+        print("Match! Both file hashes are the same.")
+    else:
+        print("Uh oh! These file hashes don't match. Ensure you selected the right file and inserted the correct hash.")
