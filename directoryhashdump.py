abf147c6c1feee6d668d7a7f8fd8938ffec17aa9
Ethan Chumley <144408377+echumley@users.noreply.github.com>
2024-09-23 21:19:01 -0700

    Inital commit of directory hash dump script

---
+++ directoryhashdump.py
@@ -0,0 +1,38 @@
+import hashlib
+import os
+import pathlib
+
+# Get User Input
+dirPath = input('What directory would you like to scan?: ')
+dirPath = pathlib.Path(dirPath)
+fileNum = 0
+
+try: # Using a try block instead of a while loop due to its graceful error handling and better file handling
+    if dirPath.is_dir(): # Checks to see if the directory is valid before continuing script
+        print(f'Walking: {dirPath.absolute()}....')
+        print('=' * 90)
+        try:
+            dirDict = {}
+            for item in dirPath.iterdir(): # Iterates through each item in the directory
+                if fileNum >= 100: # Breaks out of the loop if the file count reaches 100
+                    print("File limit reached. Exiting loop...")
+                    break
+                
+                if item.is_file():
+                    fileNum += 1
+
+                    # Generates the hashes of each file's contents
+                    with open(item, 'rb') as file:
+                        fileContents = file.read()
+                        sha256 = hashlib.sha256()
+                        sha256.update(fileContents)
+                        hashedFile = sha256.hexdigest()
+
+                    print(hashedFile)
+                    
+        except Exception as err:
+            print(f'ERROR: {err}')
+    else:
+        print("INVALID DIRECTORY - This directory doesn't exist.")
+except Exception as er:
+    print("INVALID DIRECTORY - This directory doesn't exist.")
